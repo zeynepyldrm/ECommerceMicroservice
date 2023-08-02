c1aa5694f2fd47ea29f79e567ff2b56e56186237
Zeynep Yıldırım <zeynepyldrm1516@gmail.com>
2023-08-02 22:31:28 +0300

    initialize commit

---
+++ src/main/java/com/example/ecommercemicroservice/ECommerceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommercemicroservice/ECommerceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
