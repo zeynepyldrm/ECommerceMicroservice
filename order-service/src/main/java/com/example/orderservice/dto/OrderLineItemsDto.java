59986e8b0992500c8a624d25d3cd570fe7b05894
zeynepyldrm <zeynepyldrm1516@gmail.com>
2023-08-09 13:50:21 -0700

    order-service implemented

---
+++ order-service/src/main/java/com/example/orderservice/dto/OrderLineItemsDto.java
@@ -0,0 +1,19 @@
+package com.example.orderservice.dto;
+
+import com.example.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderLineItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+    OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream().map(this::mapToDto).toList();
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setQuantity(orderLineItems.getQuantity());
+        orderLineItems.setSkuCode(orderLineItems.getSkuCode());
+        return orderLineItems;
+    }
+
+}
