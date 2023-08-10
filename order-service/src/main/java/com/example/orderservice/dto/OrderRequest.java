4ea5e31f1dfabeabb0f4f9b954b3ff6f84074f73
zeynepyldrm <zeynepyldrm1516@gmail.com>
2023-08-10 00:38:09 -0700

    order-service added test

--- order-service/src/main/java/com/example/orderservice/dto/OrderRequest.java
+++ order-service/src/main/java/com/example/orderservice/dto/OrderRequest.java
@@ -1,6 +1,7 @@
 package com.example.orderservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderRequest {
--- order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.orderservice;
 
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.repository.OrderRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class OrderServiceApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void contextLoads() {
+    void shouldCreatePlaceOrder() throws Exception {
+        OrderRequest orderRequest = getMockOrderRequest();
+        String orderRequestString = objectMapper.writeValueAsString(orderRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderRequestString))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+        Assertions.assertEquals(4,orderRepository.findAll().size());
+
+    }
+
+    private OrderRequest getMockOrderRequest() {
+        List orderLines = new ArrayList();
+        Order order = new Order();
+        order.setOrderNumber("12");
+        orderLines.add(order);
+
+        return OrderRequest.builder().orderLineItemsDtoList(orderLines).build();
     }
 
 }
