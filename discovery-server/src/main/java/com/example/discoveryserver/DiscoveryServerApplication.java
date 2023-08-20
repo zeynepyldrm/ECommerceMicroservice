ed7d5d81915fe74508e0af32427bfba323909626
zeynepyldrm <zeynepyldrm1516@gmail.com>
2023-08-20 18:29:34 +0300

    keycloak implementation

--- discovery-server/src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
@@ -2,7 +2,6 @@ package com.example.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
