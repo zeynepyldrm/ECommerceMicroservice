0f0cdaa83be098eacda40373a7767e609145525f
zeynepyldrm <zeynepyldrm1516@gmail.com>
2023-08-20 15:53:08 +0300

    api-gateway up and running

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,13 @@
-package com.example.productservice;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class ProductServiceApplication {
-
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
